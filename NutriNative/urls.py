2f0843afddf6baedc937a31ef534b326d080cc88
Srimann Ramachandruni <srimann.ramachandruni@gmail.com>
2025-05-24 22:35:16 -0400

    Clean home page

--- NutriNative/urls.py
+++ NutriNative/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("advisor/", include("advisor.urls"))
+    path("", include("advisor.urls"))
 ]
